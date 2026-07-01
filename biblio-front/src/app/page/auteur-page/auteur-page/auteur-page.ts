b6d8ff7efe44ce735c7eb397bebe6f2ae1edc8f1
Ophicius2 <114910037+Ophicius@users.noreply.github.com>
2026-07-01 16:55:06 +0200

    un peu de angular
    yayyy

---
+++ biblio-front/src/app/page/auteur-page/auteur-page/auteur-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auteur-page',
+  imports: [],
+  templateUrl: './auteur-page.html',
+  styleUrl: './auteur-page.css',
+})
+export class AuteurPage {
+
+}
