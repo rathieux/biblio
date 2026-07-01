8c4d992e071196aa06ae4f88fbba00ed53521681
Ophicius2 <114910037+Ophicius@users.noreply.github.com>
2026-07-01 18:16:23 +0200

    La goattt

--- biblio-front/src/app/app.routes.ts
+++ biblio-front/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CollectionPage } from './page/collection-page/collection-page';
 import { LoginPage } from './page/login-page/login-page';
 import { authGuard } from './guard/auth-guard';
 import { EditeurPage } from './page/editeur-page/editeur-page';
+import { AuteurPage } from './page/auteur-page/auteur-page/auteur-page';
 
 export const routes: Routes = [
   {
@@ -13,7 +14,7 @@ export const routes: Routes = [
 
    {
     path: 'auteur',
-    component: CollectionPage,
+    component: AuteurPage,
     canActivate: [authGuard],
   },
 
