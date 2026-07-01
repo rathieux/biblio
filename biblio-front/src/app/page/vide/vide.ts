376f67be970feffc5c4e914f4e161dad196ef47c
Mathieu Legrand <mathieu.legrand78@gmail.com>
2026-07-01 19:28:21 +0200

    ca fonctionne aller ciao

--- biblio-front/src/app/page/vide/vide.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-vide',
-  imports: [],
-  templateUrl: './vide.html',
-  styleUrl: './vide.css',
-})
-export class Vide {}
