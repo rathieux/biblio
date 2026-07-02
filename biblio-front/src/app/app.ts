b7cc5a34f1e7523bc2c0a42b4d714ba39126fc92
MwanPygmay <matt.brunot@laposte.net>
2026-07-02 11:14:34 +0200

    add styling for login, add logout, add token timeout

--- biblio-front/src/app/app.ts
+++ biblio-front/src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from './service/auth-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('biblio-front');
+
+  constructor(
+    protected authService: AuthService,
+    private router: Router
+  ) {}
+
+  logOut() {
+    this.authService.disconnect();
+    this.router.navigate(['/login']);
+  }
 }
--- biblio-front/src/app/service/auth-service.ts
+++ biblio-front/src/app/service/auth-service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { AuthRequest } from '../model/auth-request';
+import { AuthRequest } from '../model/auth-request';import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
-import { AuthResponse } from '../model/auth-response';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private _token: string = sessionStorage.getItem('token') ?? '';
+  private _token: string = '';
 
   public get token(): string {
+    const cachedToken = sessionStorage.getItem('token');
+
+    if (cachedToken) {
+      if (new Date() > new Date((jwtDecode(cachedToken).exp ?? 0) * 1000)) {
+        this.disconnect();
+      }
+      return cachedToken;
+    }
     return this._token;
   }
 
