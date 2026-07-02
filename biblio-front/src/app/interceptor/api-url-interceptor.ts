f73b773ea2a01beb8f27181b190af17f49be1b79
rathieux <rathieux@gmail.com>
2026-07-02 13:43:03 +0200

    java url

--- biblio-front/src/app/interceptor/api-url-interceptor.ts
+++ biblio-front/src/app/interceptor/api-url-interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const apiUrlInterceptor: HttpInterceptorFn = (req, next) => {
   const apiRequest = req.clone({
-    url: `http://localhost:8080/api${ req.url }`
+    url: `http://java:8080/api${ req.url }`
   });
 
   return next(apiRequest);
