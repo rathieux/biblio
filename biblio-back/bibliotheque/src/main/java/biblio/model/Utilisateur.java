22e2b6bb5707d971971b892934181eb60a99a921
MwanPygmay <matt.brunot@laposte.net>
2026-07-02 14:53:32 +0200

    substring problem fixed

--- biblio-back/bibliotheque/src/main/java/biblio/model/Utilisateur.java
+++ biblio-back/bibliotheque/src/main/java/biblio/model/Utilisateur.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "utilisateur")
 public class Utilisateur {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
